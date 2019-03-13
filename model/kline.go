ecb008f19bf7b0de19040a3c8c0b5df8d3abfe5c
王彬彬 <769558579@qq.com>
2019-03-13 17:51:20 +0800

    K线入库逻辑

--- model/kline.go
+++ model/kline.go
@@ -1,7 +1,7 @@
 // date: 2019-03-05
 package model
 
-type Kline struct {
+type kline struct {
 	Id     uint    `json:"id"`
 	Symbol string  `json:"symbol"`
 	Open   float64 `json:"open"`
@@ -10,4 +10,54 @@ type Kline struct {
 	Low    float64 `json:"low"`
 	Volume float64 `json:"volume"`
 	Ts     uint    `json:"ts"`
+}
+
+//分钟线
+type Kline1m struct {
+	kline
+}
+
+//5分钟K线
+type Kline5m struct {
+	kline
+}
+
+//15分钟K线
+type Kline15m struct {
+	kline
+}
+
+//30分钟K线
+type Kline30m struct {
+	kline
+}
+
+//60分钟K线
+type Kline60m struct {
+	kline
+}
+
+//4小时K线
+type Kline240m struct {
+	kline
+}
+
+//1天K线
+type Kline1440m struct {
+	kline
+}
+
+//5天K线
+type Kline7200m struct {
+	kline
+}
+
+//1周K线
+type Kline10080m struct {
+	kline
+}
+
+//1月K线
+type Kline8640m struct {
+	kline
 }
--- service/kline.go
+++ service/kline.go
@@ -1,10 +1,84 @@
 // date: 2019-03-13
 package service
 
+import (
+	"github.com/Jarvens/Exchange-Agent/db"
+	"github.com/Jarvens/Exchange-Agent/model"
+	"github.com/Jarvens/Exchange-Agent/util/log"
+)
+
 func FindKline() {
 
 }
 
-func CreateKline() {
+//K线入库失败需要将错误K线数据写入处理容器，并且需要提供工具类操作未写入成功的数据
 
+//分钟线
+func CreateKline1m(kline model.Kline1m) {
+	if err := db.ORM.Create(&kline).Error; err != nil {
+		log.Infof("分钟线入库失败: %v", err)
+	}
+}
+
+//5分钟线
+func CreateKline5m(kline model.Kline5m) {
+	if err := db.ORM.Create(&kline).Error; err != nil {
+		log.Infof("5分钟线入库失败: %v", err)
+	}
+}
+
+//15分钟线
+func CreateKline15m(kline model.Kline15m) {
+	if err := db.ORM.Create(&kline).Error; err != nil {
+		log.Infof("15分钟线入库失败: %v", err)
+	}
+}
+
+//30分钟线
+func CreateKline30m(kline model.Kline30m) {
+	if err := db.ORM.Create(&kline).Error; err != nil {
+		log.Infof("30分钟线入库失败: %v", err)
+	}
+}
+
+//小时线分钟线
+func CreateKline60m(kline model.Kline60m) {
+	if err := db.ORM.Create(&kline).Error; err != nil {
+		log.Infof("小时线入库失败: %v", err)
+	}
+}
+
+//4小时线
+func CreateKline240m(kline model.Kline240m) {
+	if err := db.ORM.Create(&kline).Error; err != nil {
+		log.Infof("4小时线入库失败: %v", err)
+	}
+}
+
+//1天线
+func CreateKline1440m(kline model.Kline1440m) {
+	if err := db.ORM.Create(&kline).Error; err != nil {
+		log.Infof("1天线入库失败: %v", err)
+	}
+}
+
+//5天线
+func CreateKline7200m(kline model.Kline7200m) {
+	if err := db.ORM.Create(&kline).Error; err != nil {
+		log.Infof("5天线入库失败: %v", err)
+	}
+}
+
+//周线
+func CreateKline10080m(kline model.Kline10080m) {
+	if err := db.ORM.Create(&kline).Error; err != nil {
+		log.Infof("周线入库失败: %v", err)
+	}
+}
+
+//月线
+func CreateKline8640m(kline model.Kline8640m) {
+	if err := db.ORM.Create(&kline).Error; err != nil {
+		log.Infof("月线入库失败: %v", err)
+	}
 }
